23c4476dd238bba2473247cba00be2a0246ca458
Jonathan-T1 <jdcaon85@misena.edu.co>
2025-07-22 00:39:00 -0500

    Adelanto estructurado Canvas

---
+++ Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Administrador de Tareas</title>
+</head>
+<body>
+  <?php include 'components/Navbar.php';?>
+  
+    <h1>prueba general</h1>
+
+</body>
+</html>
---
+++ components/Navbar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/Navbar.css">
+</head>
+<body>
+    <nav class ="preheader">
+        <div>
+            <ul>
+                <li><a href="#">Cuenta</a></li>
+                <li><a href="#">Tablero</a></li>
+                <li><a href="#">Cursos</a></li>
+                <li><a href="#">Calendario</a></li>
+                <li><a href="#">Bandeja de entrada</a></li>
+                <li><a href="#">Historial</a></li>
+                <li><a href="#">Ayuda</a></li>
+            </ul>
+        </div>
+    </nav> 
+</body>
+</html>
